f7b90d8c360521dbbcb90145de181baab96c2310
Martin Pineda <m.pineda@unillanos.edu.co>
2026-09-17 20:09:01 -0500

    Add Dockerfile for cloud deployment and update imports

--- src/main/java/com/eventu/repositories/AsistenciaRepository.java
+++ src/main/java/com/eventu/repositories/AsistenciaRepository.java
@@ -1,10 +1,9 @@
 package com.eventu.repositories;
 
-import com.eventu.models.Asistencia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.eventu.models.Asistencia;
 
 @Repository
 public interface AsistenciaRepository extends JpaRepository<Asistencia, Long> {
